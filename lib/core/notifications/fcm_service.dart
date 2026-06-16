83449f2adb567ac2ca3462d49de12185f6e3f612
MawanRequiem <dharmawan1209@gmail.com>
2026-06-16 19:50:02 +0700

    fix: resolve Priority ambiguous import conflict in FCM service

--- lib/core/notifications/fcm_service.dart
+++ lib/core/notifications/fcm_service.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter/scheduler.dart' hide Priority;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:agniraksha_mobile/core/network/api_endpoints.dart';
